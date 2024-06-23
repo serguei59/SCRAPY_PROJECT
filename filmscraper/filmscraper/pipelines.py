41ed11b4ab97b5dda1b8e0862aff8d9b816c800f
Serge Buasa <sergebuasa@gmail.com>
2024-06-23 18:29:08 +0200

    clean basique pour load propre films

--- filmscraper/filmscraper/pipelines.py
+++ filmscraper/filmscraper/pipelines.py
@@ -17,9 +17,23 @@ class FilmscraperPipeline:
     
 class FilmsAllocinescraperPipeline:
     def process_item(self, item, spider):
+        item = self.clean_annee(item)
         item = self.clean_duree(item)
+        item = self.clean_langue(item)
+        item = self.clean_realisateur(item)
+        item = self.clean_title(item)
+        item = self.clean_titreOriginal(item)
         return item
 
+    def clean_annee(self,item):
+        adapter = ItemAdapter(item)
+        annee = adapter.get('annee')
+        print(annee)
+        #supprimer le /n 1
+        cleaned_annee = annee.strip()
+        print(cleaned_annee)
+        adapter['annee']= cleaned_annee
+        return item
 
     def clean_duree(self,item):
         adapter = ItemAdapter(item)
@@ -32,6 +46,36 @@ class FilmsAllocinescraperPipeline:
         cleaned_duree = h*60 + mn
         adapter['duree'] = cleaned_duree
         return item
+    
+    def clean_langue(self,item):
+        adapter = ItemAdapter(item)
+        langue = adapter.get('langueOrigine')
+        cleaned_langue = langue.strip()
+        adapter['langueOrigine'] = cleaned_langue
+        return item
+    
+    def clean_realisateur(self,item):
+        adapter = ItemAdapter(item)
+        realisateur = adapter.get('realisateur')
+        cleaned_realisateur = realisateur.strip()
+        adapter['realisateur'] = cleaned_realisateur
+        return item
+    
+    def clean_title(self,item):
+        adapter = ItemAdapter(item)
+        title = adapter.get('title')
+        cleaned_title = title.strip()
+        adapter['title'] = cleaned_title
+        return item
+    
+    def clean_titreOriginal(self,item):
+        adapter = ItemAdapter(item)
+        titreOriginal = adapter.get('titreOriginal')
+        cleaned_titreOriginal = titreOriginal.strip() if titreOriginal else titreOriginal
+        adapter['titreOriginal'] = cleaned_titreOriginal
+        return item    
+    
+    
 
 class SeriesAllocinescraperPipeline:
     def process_item(self,item,spider):
